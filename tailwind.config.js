c77e0ad9931c48ac03f3b1f6c4f622a4b898cceb
Lenfumé Julien <julien.lenfume@gmail.com>
2024-10-31 16:08:54 +0100

    init project - first commit after installation

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#0C2E8A",
+                secondary: " #546CAD",
+            },
+            fontFamily: {
+                'roboto': ['Roboto', 'sans-serif'],
+            }
+        },
+    },
+
+    plugins: [forms],
+};
